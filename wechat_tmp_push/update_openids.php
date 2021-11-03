a50a935a04f169fcf291e99f017a6758ed857aa5
fanpink <36870925+fanpink@users.noreply.github.com>
2021-11-03 11:53:37 +0800

    Delete wechat_tmp_push directory

--- wechat_tmp_push/update_openids.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Content-Type:text/html;charset=utf-8");
-require_once './push_core.php';
-$send = new push_core();
-$openid = $send->get_server_openid();
-echo $openid;
-?>
--- wechat_tmp_push/update_token.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Content-Type:text/html;charset=utf-8");
-require_once './push_core.php';
-$send = new push_core();
-$token = $send->get_server_token();
-echo $token;
-?>
